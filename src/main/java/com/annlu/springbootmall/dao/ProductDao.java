5dad65d2153f3c5a172a880913619089db66b8d5
AnnLu <jo1397200@gmail.com>
2023-10-21 20:05:08 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/annlu/springbootmall/dao/ProductDao.java
+++ src/main/java/com/annlu/springbootmall/dao/ProductDao.java
@@ -7,4 +7,6 @@ public interface ProductDao {
     Product getProductById(Integer productId);//根據productId去查詢商品數據
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId,ProductRequest productRequest);
 }
